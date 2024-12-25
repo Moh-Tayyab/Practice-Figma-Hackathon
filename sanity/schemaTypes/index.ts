5403e2275258373b7f858a0cfe4f5312be16ecb1
Muhammad-Tayyab <160000753+Moh-Tayyab@users.noreply.github.com>
2024-12-25 00:18:57 -0800

    update

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,5 +2,5 @@ import { type SchemaTypeDefinition } from 'sanity'
 import product from '../product'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product],
+  types: [product]
 }
